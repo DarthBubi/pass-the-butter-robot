3befa7497fdd17deab9cbcf06604e9919dc46042
Johannes Hackbarth <johannes.hackbarth@dfki.de>
2020-11-01 16:57:09 +0100

    Update code for ultrasonic rangefinder

--- pass_the_butter_sensors/src/ultrasonic_rangefinder/ultrasonic_rangefinder.ino
+++ pass_the_butter_sensors/src/ultrasonic_rangefinder/ultrasonic_rangefinder.ino
@@ -7,70 +7,64 @@
  Entfernung Messen und auf seriellen Schnittstelle im Sekundentakt in cm ausgeben
  */
 
-// #include <SoftwareSerial.h>
+#define __AVR_ATmega328P__
 #include <ros.h>
 #include <sensor_msgs/Range.h>
 
-#define trigger 3  // Arduino Pin an HC-SR04 Trig
-#define echo 2     // Arduino Pin an HC-SR04 Echo
-#define trigger_rear 5  // Arduino Pin an HC-SR04 Trig
-#define echo_rear 4     // Arduino Pin an HC-SR04 Echo
+const uint8_t trigger = 3;       // Arduino Pin an HC-SR04 Trig
+const uint8_t echo = 2;          // Arduino Pin an HC-SR04 Echo
+const uint8_t trigger_rear = 5;  // Arduino Pin an HC-SR04 Trig
+const uint8_t echo_rear = 4;     // Arduino Pin an HC-SR04 Echo
 
-constexpr float fov = 0.2617993877991494;
-constexpr float range_min = 0.02;
-constexpr float range_max = 3.0;
+const float fov = 0.2617993877991494;
+const float range_min = 0.02;
+const float range_max = 3.0;
 
 ros::NodeHandle nh;
 sensor_msgs::Range range_msg;
 ros::Publisher range_finder_front_pub("/front_ultrasonic", &range_msg);
 ros::Publisher range_finder_rear_pub("/rear_ultrasonic", &range_msg);
 
-// SoftwareSerial esp8266(11, 12);
-
 void setup()
 {
   pinMode(trigger, OUTPUT);
   pinMode(echo, INPUT);
-  digitalWrite(trigger, HIGH);  // Signal abschalten
+  digitalWrite(trigger, HIGH);
 
   pinMode(trigger_rear, OUTPUT);
   pinMode(echo_rear, INPUT);
-  digitalWrite(trigger_rear, HIGH);  // Signal abschalten
-
-  // Open serial communications and wait for port to open:
-  // Serial.begin(19200);
+  digitalWrite(trigger_rear, HIGH);
 
   nh.initNode();
   nh.advertise(range_finder_front_pub);
   nh.advertise(range_finder_rear_pub);
-}
 
-void loop()  // Arduino Hauptschleife
-{
-  int distance = getDistance(0);
-  // int mean = getDistanceMA();
-
-  range_msg.header.stamp = nh.now();
-  range_msg.header.frame_id = "front_sonar";
   range_msg.radiation_type = range_msg.ULTRASOUND;
   range_msg.min_range = range_min;
   range_msg.max_range = range_max;
   range_msg.field_of_view = fov;
-  range_msg.range = static_cast<float>(distance) / 100.;
+}
 
+void loop()
+{
+  int distance = getDistance(0);
+
+  range_msg.header.stamp = nh.now();
+  range_msg.header.frame_id = "front_sonar";
+  range_msg.range = static_cast<float>(distance) / 100.;
   range_finder_front_pub.publish(&range_msg);
 
   distance = getDistance(1);
+
   range_msg.header.stamp = nh.now();
   range_msg.header.frame_id = "rear_sonar";
   range_msg.range = static_cast<float>(distance) / 100.;
   range_finder_rear_pub.publish(&range_msg);
 
   nh.spinOnce();
-  delay(10);
+  delay(50);
 }
 
-// Entfernung in cm über gewöhnlichen Ultraschallsensor mit Echo und Trigger messen
 int getDistance(int sensor)
 {
   long distance = 0;
@@ -78,45 +72,26 @@ int getDistance(int sensor)
 
   if (sensor == 0)
     digitalWrite(trigger, LOW);
-  else
+  else if (sensor == 1)
     digitalWrite(trigger_rear, LOW);
-  delayMicroseconds(3);
+  delayMicroseconds(5);
   noInterrupts();
 
   if (sensor == 0)
-    digitalWrite(trigger, HIGH);  // Trigger Impuls 10 us
-  else
+    digitalWrite(trigger, HIGH);  // Trigger Impuls 50 us
+  else if (sensor == 1)
     digitalWrite(trigger_rear, HIGH);
-  delayMicroseconds(10);
-  
-  if (sensor == 0)
-    digitalWrite(trigger, LOW);
-  else
-    digitalWrite(trigger_rear, LOW);
+  delayMicroseconds(50);
 
-  time = sensor == 0 ? pulseIn(echo, HIGH) : pulseIn(echo_rear, HIGH);  // Echo-Zeit messen
+  if (sensor == 0)
+    time = pulseIn(echo, HIGH);
+  else if (sensor == 1)
+    time = pulseIn(echo_rear, HIGH);
+
+  // time = sensor == 0 ? pulseIn(echo, HIGH) : pulseIn(echo_rear, HIGH);  // Echo-Zeit messen
   interrupts();
 
   time = (time / 2);       // Zeit halbieren
   distance = time / 29.1;  // Zeit in Zentimeter umrechnen
   return distance;
 }
-
-// Entfernung Gleitender Durchschnitt (gewichtet)
-/* int getDistanceMA()
-{
-  int old = 0;
-  int avg, dist;
-
-  delay(10);
-  old = getDistance(0);
-  delay(10);
-  for (int i = 0; i < 10; i++)
-  {
-    dist = getDistance(0);
-    avg = (0.8 * old) + (0.2 * dist);
-    old = avg;
-    delay(10);
-  }
-  return avg;
-} */
